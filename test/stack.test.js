292b0f189e9e5c857434f7c4e70d9df016937157
StephaneNguyen <81899264+stephane-nguyen@users.noreply.github.com>
2022-06-16 11:17:53 +0200

    Update stack.test.js

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,13 +1,3 @@
-//added a npm cmd to execute the js test runner in the package.json: npm run test 
-//"test": "jest --watchAll --verbose"
-
-//  add types to the project to give us intellisense for all the matchers built into jest
-// to do so, run npm i @types/jest and create jsconfig.json
-
-// add --coverage to     "test": "jest --watchAll --verbose"
-// 100% code coverage != good test suite, good thing to show
-
-
 //test a stack without arrays 
 class Stack {
     constructor() {
